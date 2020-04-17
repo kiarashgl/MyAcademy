f31afb847268b21abbf9d1204df80000edc9547f
Amir Mojtaba Sabour <amir.mojtaba@yahoo.com>
2020-04-17 18:59:38 +0430

    Add Login view
    Added new Login view class to be consistent.
    Added the app_name for the accounts app.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from django.views.generic import TemplateView
-
 from . import views
 
+app_name = 'accounts'
 urlpatterns = [
+	path('login/', views.Login.as_view(), name='login'),
 	path('signup/', views.SignUp.as_view(), name='signup'),
 	path('', include('django.contrib.auth.urls')),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,14 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views import generic
 from django.shortcuts import render
 
 from .forms import SignupForm
+
+
+class Login(LoginView):
+	success_url = reverse_lazy('home')
+	template_name = 'registration/login.html'
 
 
 class SignUp(generic.CreateView):
