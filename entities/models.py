0afffb8cf4300cb95252ab832f20c736e0d9de90
Amir Mojtaba Sabour <amir.mojtaba@yahoo.com>
2020-05-06 00:33:22 +0430

    Add pictures for entities
    Updated entity models to include pictures.
    Updated entity models to use foreign keys to reference each other.

--- entities/models.py
+++ entities/models.py
@@ -3,22 +3,18 @@ from django.db import models
 
 class Entity(models.Model):
 	verified = models.BooleanField(default=False)
+	picture = models.ImageField(upload_to='profile_pictures', null=True)
+
+	def __str__(self):
+		raise NotImplementedError
 
 
 class Professor(Entity):
 	first_name = models.CharField(max_length=30, blank=False)
 	last_name = models.CharField(max_length=30, blank=False)
+	my_department = models.ForeignKey('Department', on_delete=models.SET_NULL, null=True)
 
-	# picture = models.ImageField(upload_to='profile_pictures', default='default_profile_picture.png') # TODO: Will do later
-
-	department_name = models.CharField(max_length=30,
-	                                   blank=True)  # TODO: Fix later and add in a reference to a department
-	university_name = models.CharField(max_length=30,
-	                                   blank=True)  # TODO: Fix later and add in a reference to a university
-
-	score = models.IntegerField()  # Between 0-10 # TODO: Fix later (Must be computed or some shit in the next sprints)
 	# tags = None # TODO: Fix later
-	pass
 
 	def __str__(self):
 		return self.first_name + " " + self.last_name
@@ -26,10 +22,7 @@ class Professor(Entity):
 
 class Department(Entity):
 	name = models.CharField(max_length=30, blank=False)
-	university_name = models.CharField(max_length=30,
-	                                   blank=False)  # TODO: Fix later and add in a reference to a university
-
-	score = models.IntegerField()  # Between 0-10 # TODO: Fix later (Must be computed or some shit in the next sprints)
+	my_university = models.ForeignKey('University', on_delete=models.SET_NULL, null=True)
 
 	def __str__(self):
 		return self.name
