5aaba465882e3e5d481679f55c5f021610c7978a
Amir Mojtaba Sabour <amir.mojtaba@yahoo.com>
2020-05-08 03:11:56 +0430

    Make profile pages prettier, Delete homepage and replace with core
    Deleted the homepage app which wasn't necessary so far and moved all
    the templates to the core app.
    
    Made the default profile pages of the entities a bit more prettier.

--- MyAcademy/settings.py
+++ MyAcademy/settings.py
@@ -29,7 +29,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-	'homepage',
 	'accounts',
 	'entities',
 	'core',
--- MyAcademy/urls.py
+++ MyAcademy/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from . import settings
 
 urlpatterns = [
-	path('', include('homepage.urls')),
+	path('', include('core.urls')),
 	path('accounts/', include('accounts.urls')),
 	path('entities/', include('entities.urls')),
 	path('admin/', admin.site.urls),
