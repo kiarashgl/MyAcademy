cf0fa71293ac33409ea721f83676c63d75cad469
Kiarash Golzadeh <kiarash.gl@gmail.com>
2020-05-20 02:15:57 +0430

    Settings for Deployment

--- MyAcademy/settings.py
+++ MyAcademy/settings.py
@@ -19,12 +19,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'MyAcademy'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'MyAcademy')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['EdenHazard.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
@@ -83,6 +83,9 @@ DATABASES = {
 		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 	}
 }
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 LOGIN_URL = 'accounts.login'
 LOGIN_REDIRECT_URL = 'home'
