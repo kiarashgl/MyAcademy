477b3f2dcf58f9de23b34e567c56b54674f1555b
Kiarash Golzadeh <kiarash.gl@gmail.com>
2020-05-28 00:22:34 +0430

    Remove local settings auto detect

--- MyAcademy/settings.py
+++ MyAcademy/settings.py
@@ -19,12 +19,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'MyAcademy')
+SECRET_KEY = 'MyAcademy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 # Application definition
 
