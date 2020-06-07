3881d406bdbe796f3b233db46f8f64eb9f1e28b0
Kiarash Golzadeh <kiarash.gl@gmail.com>
2020-06-08 02:13:44 +0430

    Remove deployment from Travis

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-ssh ubuntu@myacademysut.ir
-cd ~/MyAcademy
-git pull --rebase origin master
-source venv/bin/activate
-pip install -r requirements.txt
-python manage.py migrate --no-input
-python manage.py collectstatic
-sudo systemctl gunicorn restart
