bb11378c7b6a0285e6926ff918a3ae64576f4608
Amir Mojtaba Sabour <amir.mojtaba@yahoo.com>
2020-06-08 19:15:08 +0430

    Make the comments visible

--- entities/templatetags/comment_tag.py
+++ entities/templatetags/comment_tag.py
@@ -1,10 +1,14 @@
 from django import template
 
-
 register = template.Library()
+
 
 @register.inclusion_tag('comment.html')
 def render_comment(rating):
-	return {'content': rating.comment, 'first_name': rating.user.first_name,
-										'last_name': rating.user.last_name,
-										'date': rating.date}
+	return {
+		'content': rating.comment,
+		'first_name': rating.user.first_name,
+		'last_name': rating.user.last_name,
+		'picture': rating.user.profile_picture.url,
+		'date': rating.date,
+	}
