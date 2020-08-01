e0167b917fa0a843e5be8d4ddcc4197dfa4d85af
alirezamoosio <moosavi78@gmail.com>
2020-08-02 02:26:40 +0430

    Add a link to cms-admin and make every new user and editor

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.translation import ugettext as _
 
 
@@ -20,3 +22,9 @@ class User(AbstractUser):
 
 	def __str__(self):
 		return self.username
+
+
+@receiver(post_save, sender=User)
+def add_to_blog_editors(sender, instance, created, **kwargs):
+	if created:
+		instance.groups.add(Group.objects.get(name='Editors'))
