83e828f9a5b7f784a8a21ee819c9c959f40fd05e
Kiarash Golzadeh <kiarash.gl@gmail.com>
2020-08-05 12:58:10 +0430

    Add random avatar

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,10 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.translation import ugettext as _
 
+import hashlib
+
+IDENTICON_API_URL = "https://identicon-api.herokuapp.com/%s/512?format=png"
+
 
 class User(AbstractUser):
 	# Authentication fields
@@ -15,13 +19,23 @@ class User(AbstractUser):
 
 	# Extra profile fields
 	profile_picture = models.ImageField("تصویر نمایه", upload_to='profile_pictures',
-										default='profile_pictures/default_profile_picture.jpg')
+										null=True, blank=True)
 	bio = models.TextField("بیوگرافی", blank=True)
 	university = models.CharField("دانشگاه", max_length=100, blank=True)
 	major = models.CharField("رشته", max_length=100, blank=True, )
 
 	def __str__(self):
 		return self.username
+
+	@property
+	def get_profile_picture_url(self):
+		try:
+			if self.profile_picture is not None:
+				return self.profile_picture.url
+		except Exception:
+			return IDENTICON_API_URL % (hashlib.md5(self.username.encode('utf-8')).hexdigest())
+
+		return IDENTICON_API_URL % (hashlib.md5(self.username.encode('utf-8')).hexdigest())
 
 
 @receiver(post_save, sender=User)
